1493abe2361e350ed44393f5fdd2b36f333ac4af
BikumallaPrudhviRaj <prudhvibikumalla@gmail.com>
2017-03-08 22:43:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
+
 var element=document.getElementById("main-text");
 element.innerText ='Helloo';
+
+var element=document.getElementById("madi");
+madi.onclick =function(){
+    img.style.marginleft='100px';
+};
