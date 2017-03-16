da32d8ccef92167dab0ed88b75a8b9f684a99595
BikumallaPrudhviRaj <prudhvibikumalla@gmail.com>
2017-03-17 00:05:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,15 @@
 var button =document.getElementById('counter');
 var counter =0;
 button.onclick =function(){
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    var request =new XMLHttprequest();
+    request.onreadystatechange = function(){
+        if(request.readystate===XMLHttprequest.DONE){
+            if(requset.status===200){
+                var counter = requset.responseText;
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http:http://bikumallaprudhviraj.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
