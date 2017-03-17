0e27f0e130f972929b8b57c04a8a62cbcc995a0a
BikumallaPrudhviRaj <prudhvibikumalla@gmail.com>
2017-03-18 00:28:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
     user:'bikumallaprudhviraj',
     database:'bikumallaprudhviraj',
     host:'db.imad.hasura-app.io',
-    port:'5432',
+    port:'8080',
     password: process.env.DB_PASSWORD
 };
 var app = express();
