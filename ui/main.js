cacdf1715d99b4d68b5bb4750b72e065d65ee9d6
BikumallaPrudhviRaj <prudhvibikumalla@gmail.com>
2017-03-18 00:45:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,8 +13,6 @@ button.onclick =function(){
     request.open('GET','http:http://bikumallaprudhviraj.imad.hasura-app.io/counter',true);
     request.send(null);
 };
-var nameInput= document.getElementById('name');
-var name =nameInput.value;
 var submit =document.getElementById('submit_btn');
 submit.onclick=function(){
     var request =new XMLHttprequest();
@@ -32,6 +30,8 @@ submit.onclick=function(){
             }
         }
     };
+    var nameInput= document.getElementById('name');
+    var name =nameInput.value;
     request.open('GET','http:http://bikumallaprudhviraj.imad.hasura-app.io/submit-name?name= '+name,true);
     request.send(null);
 };
