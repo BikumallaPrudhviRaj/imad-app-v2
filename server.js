3c7703af7b56abef45c57db8332e9622b0debd46
BikumallaPrudhviRaj <prudhvibikumalla@gmail.com>
2017-03-20 19:47:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,30 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool= require('pg').Pool;
 
+var app = express();
+app.use(morgan('combined'));
+
+var config={
+    user:'bikumallaprudhviraj',
+    database:'bikumallaprudhviraj',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:'db-bikumallaprudhviraj-16524',
+};
+
+var pool=new Pool(config);
+app.get('test-db',function(req,res){
+    //make a select request
+    //return a response with the results
+    pool.query('SELECT * from test',function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else{
+           res.send(JSON.stringify(result.rows));
+       }
+    });
+});
+
 var articles={
     'article-one' :{
         title:'ARTICLE ONE',
