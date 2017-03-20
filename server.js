c0f1a4dc63e0ed3f05b8c7909199d260526dcd1b
BikumallaPrudhviRaj <prudhvibikumalla@gmail.com>
2017-03-20 19:56:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config={
     database:'bikumallaprudhviraj',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:'db-bikumallaprudhviraj-16524',
+    password:'db-bikumallaprudhviraj-16524'
 };
 
 var pool=new Pool(config);
